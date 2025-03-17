c4a050f1b0adbe26755c86a648bae72126ed2e87
krypek <krypek@tuta.io>
2025-03-17 21:17:48 +0100

    Fix error when using crossnode

--- src/instance.ts
+++ src/instance.ts
@@ -354,7 +354,8 @@ export function injectInstance() {
     cursorFix()
 }
 function cursorFix() {
-    const sheet = [...document.styleSheets].find(sheet => sheet.href!.endsWith('game/page/game-base.css'))!
+    const sheet = [...document.styleSheets].find(sheet => sheet.href!.endsWith('game/page/game-base.css'))
+    if (!sheet) return
     for (const rule of sheet.cssRules) {
         if ('selectorText' in rule && typeof rule.selectorText == 'string' && rule.selectorText.startsWith('#game')) {
             rule.selectorText = rule.selectorText.replace(/#game/, '[id^="game"]')
