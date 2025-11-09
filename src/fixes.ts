201d9c41ef3c1156f5f759cff19902c6cb855fb9
krypek <krypek@tuta.io>
2025-11-09 18:41:18 +0100

    Fix nwjs fullscreen query lag

--- src/fixes.ts
+++ src/fixes.ts
@@ -9,6 +9,7 @@ export function injectFixes() {
     dialogFix()
     musicFix()
     optionModelFix()
+    nwjsFullscreenFix()
 }
 
 declare global {
@@ -215,6 +216,44 @@ function optionModelFix() {
         init() {
             this.parent()
             defineModEnabledProperties(this)
+        },
+    })
+}
+
+function nwjsFullscreenFix() {
+    const nwGui = window.require('nw.gui')
+    let win: any
+    function queryNwGui(): boolean {
+        win ??= nwGui.Window.get()
+        return win.isFullscreen
+    }
+
+    let isFullscreenCache = queryNwGui()
+    let lastChecked = Date.now()
+    function isFullscreenCached() {
+        const now = Date.now()
+        if (lastChecked + 10e3 < now) {
+            isFullscreenCache = queryNwGui()
+        }
+        lastChecked = now
+        return isFullscreenCache
+    }
+
+    sc.OptionModel.inject({
+        _setFullscreen() {
+            if (ig.platform == ig.PLATFORM_TYPES.DESKTOP) {
+                const newValue = this.values.fullscreen
+                localStorage.setItem('IG_FULLSCREEN', `${newValue}`)
+
+                const oldValue = isFullscreenCached()
+                if (oldValue != newValue) {
+                    if (newValue) {
+                        win.enterFullscreen()
+                    } else {
+                        win.leaveFullscreen()
+                    }
+                }
+            }
         },
     })
 }
