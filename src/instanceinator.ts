454c0864a10eb2a03bc6286a0b3f6eaa486d67fb
krypek <krypek@tuta.io>
2026-04-18 16:00:09 +0200

    Move Instanceinator class definition to a seperate file

---
+++ src/instanceinator.ts
@@ -0,0 +1,90 @@
+import type {} from 'crossnode/crossnode.d.ts'
+import { InstanceinatorInstance } from './instance'
+import { retile } from './tiler'
+import { copyInstance, type InstanceinatorCopyInstanceConfig } from './instance-copy'
+import { FpsLabelDrawClass, IdLabelDrawClass, type LabelDrawClass } from './label-draw'
+import { setMusicInstanceId } from './fixes/music-fix'
+import { classes } from './custom-classes'
+
+declare global {
+    var instanceinator: Instanceinator
+    namespace NodeJS {
+        interface Global {
+            instanceinator: Instanceinator
+        }
+    }
+}
+
+export class Instanceinator {
+    id: number = 0
+    instances: Record<number, InstanceinatorInstance> = {}
+    currentInstanceFocus: number = 0
+    idCounter: number = 0
+    displayId: boolean = false
+    displayFps: boolean = false
+    musicInstanceId: number = 0
+    cachedInstances: Record<string, Promise<InstanceinatorInstance>[]> = {}
+    allInstances: Set<InstanceinatorInstance> = new Set()
+
+    labelDrawClasses: (new (instance: InstanceinatorInstance) => LabelDrawClass)[] = [
+        IdLabelDrawClass,
+        FpsLabelDrawClass,
+    ]
+
+    resetInstanceIdCounter() {
+        if (instanceinator.allInstances.size != 1)
+            throw new Error('instanceinator.instances need to be empty when calling resetInstanceIdCounter!')
+        this.idCounter = 1
+    }
+
+    append(instance: InstanceinatorInstance) {
+        this.instances[instance.id] = instance
+        this.retile()
+    }
+
+    destroy(instance: InstanceinatorInstance) {
+        if (!this.instances[instance.id]) return
+        if (instance.id == 0) throw new Error('Cannot delete base instance with id 0!')
+        if (instanceinator.id == instance.id)
+            throw new Error(`Cannot delete currently applied instance! id: ${instance.id}`)
+
+        this.instances[instance.id].destroy()
+
+        delete this.instances[instance.id]
+        this.retile()
+    }
+
+    getAllCreatedInstances() {
+        return this.allInstances
+    }
+
+    getCachedInstanceCount(cacheKey?: string) {
+        if (cacheKey) return this.cachedInstances[cacheKey]?.length ?? 0
+        return Object.values(this.cachedInstances).reduce((acc, v) => acc + v.length, 0)
+    }
+
+    async createCachedInstances(baseInst: InstanceinatorInstance, configs: InstanceinatorCopyInstanceConfig[]) {
+        return Promise.all(
+            configs.map(async (copyConfig, i) => {
+                const cacheKey = copyConfig.cacheKey
+                if (!cacheKey) throw new Error('called createCachedInstances without a cacheKey in copyConfig!')
+                const arr = (this.cachedInstances[cacheKey] ??= [])
+                arr.push(
+                    this.copy(
+                        baseInst,
+                        {
+                            name: `cached-${cacheKey}-${Date.now()}-${i}`,
+                            display: false,
+                        },
+                        { ...copyConfig, cacheKey: undefined, noAppend: true }
+                    )
+                )
+            })
+        )
+    }
+
+    retile = retile
+    copy = copyInstance
+    setMusicInstanceId = setMusicInstanceId
+    classes!: typeof classes
+}
